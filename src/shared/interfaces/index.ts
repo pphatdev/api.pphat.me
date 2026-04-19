70cf738117a122122e80cbdcacb8aa761cf0bcd5
pphatdev <pphat.kits@gmail.com>
2026-04-19 16:54:11 +0700

    Add PaginationParams and PaginatedResult interfaces

--- src/shared/interfaces/index.ts
+++ src/shared/interfaces/index.ts
@@ -7,3 +7,21 @@ export interface Author {
 	profile: string;
 	url: string;
 }
+
+export interface PaginationParams {
+	page: number;
+	limit: number;
+	search?: string;
+	sort?: string;
+	order?: 'asc' | 'desc';
+}
+
+export interface PaginatedResult<T> {
+	data: T[];
+	pagination: {
+		page: number;
+		limit: number;
+		total: number;
+		totalPages: number;
+	};
+}
